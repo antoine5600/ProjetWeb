10ed67c4a7ad334daa12d142d37471991f354deb
Aurélie EHANNO <aurelie.ehanno56@gmail.com>
2018-09-27 19:16:59 +0200

    ajout fichier navbar et header pour eviter les repetition, ne marche pas encore

--- inscription.php
+++ inscription.php
@@ -3,91 +3,36 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--vie-->
+        <title>Topbuilder Construction Template</title>
 
-    <title>Topbuilder Construction Template</title>
+        <!-- Favicon -->
+        <link rel="icon" href="images/favicon.png" type="image/x-icon" />
+        <!-- Bootstrap CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Animate CSS -->
+        <link href="vendors/animate/animate.css" rel="stylesheet">
+        <!-- Icon CSS-->
+        <link rel="stylesheet" href="vendors/font-awesome/css/font-awesome.min.css">
+        <!-- Camera Slider -->
+        <link rel="stylesheet" href="vendors/camera-slider/camera.css">
+        <!-- Owlcarousel CSS-->
+        <link rel="stylesheet" type="text/css" href="vendors/owl_carousel/owl.carousel.css" media="all">
 
-    <!-- Favicon -->
-    <link rel="icon" href="images/favicon.png" type="image/x-icon" />
-    <!-- Bootstrap CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- Animate CSS -->
-    <link href="vendors/animate/animate.css" rel="stylesheet">
-    <!-- Icon CSS-->
-    <link rel="stylesheet" href="vendors/font-awesome/css/font-awesome.min.css">
-    <!-- Camera Slider -->
-    <link rel="stylesheet" href="vendors/camera-slider/camera.css">
-    <!-- Owlcarousel CSS-->
-    <link rel="stylesheet" type="text/css" href="vendors/owl_carousel/owl.carousel.css" media="all">
+        <!--Theme Styles CSS-->
+        <link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
 
-    <!--Theme Styles CSS-->
-    <link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-
-
-
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.min.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
-</head>
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="js/html5shiv.min.js"></script>
+        <script src="js/respond.min.js"></script>
+        <![endif]--></head>
 
 <body>
-    <!-- Preloader -->
-    <div class="preloader"></div>
-
-    <!-- Header_Area -->
-    <nav class="navbar navbar-default header_aera" id="main_navbar">
-        <div class="container">
-            <!-- searchForm --> 
-            <div class="searchForm">
-                <form action="#" class="row m0">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-search"></i></span>
-                        <input type="search" name="search" class="form-control" placeholder="Type & Hit Enter">
-                        <span class="input-group-addon form_hide"><i class="fa fa-times"></i></span>
-                    </div>
-                </form>
-            </div><!--End searchForm -->
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="col-md-2 p0">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#min_navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt=""></a>
-                </div>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="col-md-10 p0">
-                <div class="collapse navbar-collapse" id="min_navbar">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="dropdown submenu">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Menu</a>
-                            <ul class="dropdown-menu">
-                                <li><a href="parpaing.html">parpaing</a></li>
-                                <li><a href="ciment.html">Ciment</a></li>
-                                <li><a href="commandes.html">Mes commandes</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Magasin</a></li>
-                        <li><a href="inscripiton.php">Mon compte</a></li>
-                        <li><a href="contact.php">Contact</a></li>
-                        <li><a href="#" class="nav_searchFrom"><i class="fa fa-search"></i></a></li>
-                        <li><a href="#" class="panier"><i class="fas fa-cart-arrow-down"></i></a></li>
-
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div>
-        </div><!-- /.container -->
-    </nav>
-    <!-- End Header_Area -->
+    <?php include('navbar.php'); ?>
 
     <!-- Collect information for inscription -->
     <section class="all_contact_info">
