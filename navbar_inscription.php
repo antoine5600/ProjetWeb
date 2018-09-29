e9f4786d598718fbf0acb009027e770dcc39e0dc
Aurélie EHANNO <aurelie.ehanno56@gmail.com>
2018-09-29 19:56:05 +0200

    modif navbar inscrption

--- navbar_inscription.php
+++ navbar_inscription.php
@@ -5,7 +5,7 @@
     <!-- Header_Area -->
     <nav class="navbar navbar-default header_aera" >
         <div class="container">
-            <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt=""></a>
+            <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt=""></a>
         </div><!-- /.container -->
     </nav>
     <!-- End Header_Area -->
