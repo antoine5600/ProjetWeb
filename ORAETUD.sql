3f0bbe875acaca1b32e4d1bd1b3ad6ddbc802d8f
BobTheBlobz <43565180+BobTheBlobz@users.noreply.github.com>
2018-10-02 09:59:25 +0200

    Add files via upload
    last_final_version_2.0.2_bdd_END

--- ORAETUD.sql
+++ ORAETUD.sql
@@ -12,6 +12,7 @@ drop table Clients;
 drop table Users;
 drop table Addresses;*/
 
+
 create table Addresses( id_addr integer NOT NULL AUTO_INCREMENT, 
                         Street varchar(128), 
                         Additional varchar(128), 
@@ -23,35 +24,22 @@ create table Addresses( id_addr integer NOT NULL AUTO_INCREMENT,
 
 
 create table Users( id_usr integer NOT NULL AUTO_INCREMENT, 
-                    Name varchar(128), 
-                    First_name varchar(128), 
-                    Mail varchar(128), 
-                    Psswd char(128),
+                    Name varchar(128) NOT NULL, 
+                    First_name varchar(128) NOT NULL, 
+                    Mail varchar(128) NOT NULL, 
+                    Psswd char(128) NOT NULL,
+                    Telephone varchar(12),
+					User_permission integer NOT NULL,
+					User_sex char(1),
+					User_Bday date,
                     CONSTRAINT PK_usr PRIMARY KEY (id_usr)
-);
-
-
-create table Clients(   id_client integer NOT NULL, 
-                        Telephone varchar(128),
-                        CONSTRAINT PK_client PRIMARY KEY (id_client),
-                        CONSTRAINT FK_client FOREIGN KEY (id_client) REFERENCES Users(id_usr)
-);
-
-
-create table Admins(     id_admin integer NOT NULL,
-                        CONSTRAINT PK_admin PRIMARY KEY (id_admin),
-                        CONSTRAINT FK_admin FOREIGN KEY (id_admin) REFERENCES Users(id_usr)
-);
-
-
-create table Mods(      id_mod integer NOT NULL,
-                        CONSTRAINT PK_mod PRIMARY KEY (id_mod),
-                        CONSTRAINT FK_mod FOREIGN KEY (id_mod) REFERENCES Users(id_usr)
+					CONSTRAINT CHK_usr CHECK (User_permission<=3 AND User_permission>=0)
 );
 
 
 create table Products(  id_prod integer NOT NULL AUTO_INCREMENT, 
                         Name varchar(256), 
+						Description varchar(512),
                         Price real, 
                         Stock integer, 
                         Sales real,
@@ -62,11 +50,11 @@ create table Products(  id_prod integer NOT NULL AUTO_INCREMENT,
 
 create table Categories(    id_cat integer NOT NULL AUTO_INCREMENT, 
                             Name varchar(64), 
+							Description varchar(512),
                             parent_category integer,
                             CONSTRAINT PK_cat PRIMARY KEY (id_cat),
                             CONSTRAINT FK_cat FOREIGN KEY (parent_category) REFERENCES Categories(id_cat)
 );
-
 
 
 create table Product_list(  id_list integer NOT NULL AUTO_INCREMENT,
@@ -82,8 +70,6 @@ create table Product_quantity(  list integer NOT NULL,
                                 CONSTRAINT FK_prod_qty_prod FOREIGN KEY (product) REFERENCES Products(id_prod),
                                 CONSTRAINT CHK_prod_qty CHECK (Quantity>=0)
 );
-
-
 
 
 create table Command(   id_command integer NOT NULL AUTO_INCREMENT, 
@@ -104,9 +90,9 @@ create table Favorites( id_fav integer NOT NULL AUTO_INCREMENT,
 );
 
 
-
 create table Client_addr(   Client integer NOT NULL, 
                             Address integer NOT NULL,
+							Name varchar(64),
                             CONSTRAINT PK_client_addr PRIMARY KEY (Client, Address),
                             CONSTRAINT FK_client_addr_c FOREIGN KEY (Client) REFERENCES Clients(id_client),
                             CONSTRAINT FK_client_addr_a FOREIGN KEY (Address) REFERENCES Addresses(id_addr)
@@ -119,6 +105,7 @@ create table Product_category(  Product integer NOT NULL,
                                 CONSTRAINT FK_prod_cat_p FOREIGN KEY (Product) REFERENCES Products(id_prod),
                                 CONSTRAINT FK_prod_cat_c FOREIGN KEY (Category) REFERENCES Categories(id_cat)
 );
+
 
 create unique index I_passwords on Users (Mail, Psswd);
 create index I_products on Product_category (Product);
