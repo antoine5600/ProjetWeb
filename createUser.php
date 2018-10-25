5eac34beaad6f258c33492617e8da8634c57839d
Aurélie EHANNO <aurelie.ehanno56@gmail.com>
2018-10-25 09:57:58 +0200

    front dash

--- createUser.php
+++ createUser.php
@@ -1,28 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+</head>
+<body>
+    <?php include ('headerDash.php'); ?>
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse"><span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span></button>
+            <a class="navbar-brand" onclick="history.go(-1);" style="cursor:pointer;" ><span>Retour</span></a></br>
+        </div>
+    </div><!-- /.container-fluid -->
+</nav>
+<p></p>
+
 <?php include('server.php') ?>
 <form method="post" action="userManage.php" class="form-inline contact_box">
     <?php include('errors_reg.php'); ?>
-    <label class="main-label" for="bday">Civilit&eacute; * </label><br />
+    <label class="main-label" for="bday">Civilit&eacute; * </label>
+    <div class="sapce">
         <input type="hidden" id="civilityhidden" name="customer_title" value="1" />
         <input type="radio" id="title_1" class="active" name="customer_title" value="1" checked />
         <label class="civil" for="title_1">Madame</label>
         <input type="radio" id="title_2" name="customer_title" value="2" />
-        <label class="civil" for="title_2">Monsieur</label><br />
-    <label class="main-label" for="bday">Prénom *</label>
-        <input type="text" id="colortext" class="form-control input_box" name="userFirstname"  >
-    <label class="main-label" for="bday">Nom *</label>
-        <input type="text" id="colortext" class="form-control input_box" name="username">
-    <label class="main-label" for="bday">Email *</label>
-        <input type="text" id="colortext" class="form-control input_box" name="email">
-    <label class="main-label" for="bday">Date de naissance *</label>
-        <input type="date" id="colortext" class="form-control input_box" id="bday" name="bday">
-    <label class="main-label" for="bday">Mot de passe *</label>
-        <input type="password" id="colortext" class="form-control input_box" name="password_1">
-    <label class="main-label" for="bday">Confirmation Mot de passe *</label>
-        <input type="password" id="colortext" class="form-control input_box" type="password" name="password_2">
-    <button type="submit" class="btn btn-default" name="create_user">S'inscrire</button>
+        <label class="civil space" for="title_2">Monsieur</label>
+    </div>
+    <div class="space">
+        <label class="main-label" for="bday">Prénom *</label>
+        <input type="text" id="colortext" class="form-control input_box space" name="userFirstname" >
+    </div>
+    <div class="space">
+        <label class="main-label" for="bday">Nom *</label>
+        <input type="text" id="colortext" class="form-control input_box space" name="username">
+    </div>
+    <div class="space">
+        <label class="main-label" for="bday">Email *</label>
+        <input type="text" id="colortext" class="form-control input_box sapce" name="email">
+    </div>
+    <div class="space">
+        <label class="main-label" for="bday">Date de naissance *</label>
+        <input type="date" id="colortext" class="form-control input_box sapce" id="bday" name="bday">
+    </div>
+    <div class="space">
+        <label class="main-label" for="bday">Mot de passe *</label>
+        <input type="password" id="colortext" class="form-control input_box sapce" name="password_1">
+    </div>
+    <div class="space">
+        <label class="main-label" for="bday">Confirmation Mot de passe *</label>
+        <input type="password" id="colortext" class="form-control input_box sapce" type="password" name="password_2">
+    </div>
+    <button type="submit" class="btn btn-default" name="create_user">Ajouter</button>
     <p>* Champs obligatoires</p>
 </form>
 
-
-<button onclick="history.go(-1);">Back </button>
-
+</body>
+</html>
--- modifUser.php
+++ modifUser.php
@@ -21,32 +21,41 @@ $row = mysqli_fetch_array($result);?>
 	</div><!-- /.container-fluid -->
 </nav>
 <p></p>
-<nav class="container">
+<nav>
 <form method="post" action="userManage.php" class="form-inline contact_box">
-	<label class="main-label" for="bday">Civilit&eacute; * </label><br />
+	<div class="space">
+	<label class="main-label" for="bday">Civilit&eacute; * </label>
 	<?php if ($row['User_sex'] == '1' ): ?>
 		<input type="hidden" id="civilityhidden" name="customer_title" value="1" />
 		<input type="radio" id="title_1" class="active" name="customer_title" value="1" checked />
 		<label class="civil" for="title_1">Madame</label>
 		<input type="radio" id="title_2" name="customer_title" value="2" />
-		<label class="civil" for="title_2">Monsieur</label><br />
+		<label class="civil" for="title_2">Monsieur</label>
 	<?php else : ?>
 		<input type="hidden" id="civilityhidden" name="customer_title" value="2" />
 		<input type="radio" id="title_1"  name="customer_title" value="1" />
 		<label class="civil" for="title_1">Madame</label>
 		<input type="radio" id="title_2" class="active" name="*customer_title" value="2" checked/>
-		<label class="civil" for="title_2">Monsieur</label><br />
+		<label class="civil" for="title_2">Monsieur</label>
 	<?php endif ?>
-	<label class="main-label" for="bday">Prénom *</label>
-	<input type="text" id="colortext" class="form-control input_box" name="userFirstname" value=<?php echo $row['First_name']; ?>  ><br /><br />
-	<label class="main-label" for="bday">Nom *</label>
-	<input type="text" id="colortext" class="form-control input_box" name="username" value=<?php echo $row['Name']; ?>><br /><br />
-	<label class="main-label" for="bday">Email *</label>
-	<input type="text" id="colortext" class="form-control input_box" name="email" value=<?php echo $row['Mail']; ?>><br /><br />
-	<label class="main-label" for="bday">Date de naissance *</label>
-	<input type="date" id="colortext" class="form-control input_box" id="bday" name="bday" value=<?php echo strftime('%Y-%m-%d',
-  strtotime($row['User_Bday'])); ?> ><br /><br />
-  <input type="hidden" id="id" name="id" value=<?php echo $id; ?> />
+	</div>
+	<div class="space">
+		<label class="main-label" for="bday">Prénom *</label>
+		<input type="text" id="colortext" class="form-control input_box" name="userFirstname" value=<?php echo $row['First_name']; ?>>
+	</div>
+	<div class="space">
+		<label class="main-label" for="bday">Nom *</label>
+		<input type="text" id="colortext" class="form-control input_box" name="username" value=<?php echo $row['Name']; ?>>
+	</div>
+	<div class="space">
+		<label class="main-label" for="bday">Email *</label>
+		<input type="text" id="colortext" class="form-control input_box" name="email" value=<?php echo $row['Mail']; ?>>
+	</div>
+	<div class="space">
+		<label class="main-label" for="bday">Date de naissance *</label>
+		<input type="date" id="colortext" class="form-control input_box" id="bday" name="bday" value=<?php echo strftime('%Y-%m-%d', strtotime($row['User_Bday'])); ?> >
+  		<input type="hidden" id="id" name="id" value=<?php echo $id; ?> />
+  	</div>
 	<button type="submit" class="btn btn-default" name="edit_user">Editer</button>
 	<p>* Champs obligatoires</p>
 </form>
