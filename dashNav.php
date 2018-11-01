0f63fc4515b9893121c558eea3568d1a20a2d15a
Aurélie EHANNO <aurelie.ehanno56@gmail.com>
2018-11-02 00:48:20 +0100

    front mes commandes

--- dashNav.php
+++ dashNav.php
@@ -21,22 +21,5 @@
 		<li <?php if($page == "userManage.php"){echo 'class="active"';} ?>><a href="userManage.php"><em class="fa fa-calendar">&nbsp;</em> Gestion Utilisateur</a></li>
 		<li <?php if($page == "productManage.php"){echo 'class="active"';} ?>><a href="productManage.php"><em class="fa fa-bar-chart">&nbsp;</em> Gestion Produit</a></li>
 		<li <?php if($page == "elements.html"){echo 'class="active"';} ?>><a href="command_manage.php"><em class="fa fa-toggle-off">&nbsp;</em> Gestion Commande</a></li>
-		<li <?php if($page == "panels.html"){echo 'class="active"';} ?>><a href="panels.html"><em class="fa fa-clone">&nbsp;</em> Alerts &amp; Panels</a></li>
-		<li <?php if($page == "#"){echo 'class="active"';} ?>class="parent "><a data-toggle="collapse" href="#sub-item-1">
-			<em class="fa fa-navicon">&nbsp;</em> Multilevel <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
-		</a>
-		<ul class="children collapse" id="sub-item-1">
-			<li><a class="" href="#">
-				<span class="fa fa-arrow-right">&nbsp;</span> Sub Item 1
-			</a></li>
-			<li><a class="" href="#">
-				<span class="fa fa-arrow-right">&nbsp;</span> Sub Item 2
-			</a></li>
-			<li><a class="" href="#">
-				<span class="fa fa-arrow-right">&nbsp;</span> Sub Item 3
-			</a></li>
-		</ul>
-	</li>
-	<li <?php if($page == "login.html"){echo 'class="active"';} ?>><a href="login.html"><em class="fa fa-power-off">&nbsp;</em> Logout</a></li>
 	</ul>
 </div><!--/.sidebar-->
--- mes_commandes.php
+++ mes_commandes.php
@@ -1,13 +1,43 @@
-<?php
-	include('server_objet_a_vendre.php') ;
-	
-	if ( isset( $adresses_commandes ) == true )
-	{
-		foreach ( $adresses_commandes as $adr )
-		{
+<?php 
+  include('server_objet_a_vendre.php') ;
 ?>
-			<p> numero commande : <?php echo $adr['id_command'] . ', adresse : ' . $adr['Name'] . ' (' . $adr['Street'] . ' ' . $adr['Additional'] . ', ' . $adr['City'] . ' ' . $adr['Postcode'] . ', ' . $adr['Country'] . ')' ; ?> </p>
-<?php
-		}
-	}
-?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <?php include('navbar.php'); ?>
+    <section class="all_contact_info">
+    	<div class="container">
+            <div class="row contact_row">
+                <div class="col-sm-8 contact_info">
+                    <h2>Vos commandes</h2>
+                    <div class="location">
+						<?php 
+							if ( isset( $adresses_commandes ) == true ){
+								echo "<table class='table'> ";
+						            echo "<tr>";
+						            	echo "<th>Numéro Commande</th>";
+						                echo "<th>Adresse</th>";
+						            echo "</tr>";
+									foreach(  $adresses_commandes as $adr )
+									{
+										echo "<tr>";
+											echo '<td>' . $adr['id_command'] . '</td>';
+											echo '<td>' . $adr['Street'] . ' ' . $adr['Additional'] . ', ' . $adr['City'] . ' ' . $adr['Postcode'] . ', ' . $adr['Country'] . '</td>';
+										echo "</tr>";
+									}
+							}else{
+								echo "Aucune commande";
+							}
+				        echo "</table>";
+					?>
+                    </div>
+                </div>
+			</div>
+		</div>
+	</section>
+<?php include('footer.php'); ?>
+</body>
+</html>
+
+
